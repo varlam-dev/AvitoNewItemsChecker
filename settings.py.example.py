0b9906ef566077c81a18fcb19b4c059fac33c244
varlam_dev <varlamovserg@gmail.com>
2019-07-19 11:53:14 +0500

    добавил settings.py.example

---
+++ settings.py.example.py
@@ -0,0 +1,17 @@
+"""we write proxy server settings as http://user:password@proxy-server:port"""
+http_proxy = ""
+https_proxy = ""
+
+
+"""The key (token) is a character set of the form 110201543: AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw, 
+which is needed to receive and send messages using the Bot API."""
+telegram_token = "826596307:AAETz8qzSjnVMicNK6YOB9pIls1RN0mm2y0"
+
+"""list of telegram identifiers to whom you will send messages about new items"""
+channel_ids = ["xxxxxxxxx", "yyyyyyyyy"]
+
+"""URL for parsing"""
+url = "https://www.avito.ru/ufa/noutbuki?pmax=7000&pmin=4800&s=104"
+
+"""page refresh time for parsing in seconds"""
+update_time = 60
